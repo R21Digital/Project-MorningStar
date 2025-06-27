1fd254aaf879b54646ba03ca88f1a34f50c7b058
R21-Volvik <cjimenez@r21digital.com>
2025-06-27 10:09:16 -0700

    Add trainer OCR utilities and screen/window helpers

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,15 @@
 """Core runtime utilities."""
+
+from .trainer_ocr import (
+    preprocess_image,
+    extract_text_from_trainer_region,
+    get_untrained_skills_from_text,
+    scan_and_detect_untrained_skills,
+)
+
+__all__ = [
+    "preprocess_image",
+    "extract_text_from_trainer_region",
+    "get_untrained_skills_from_text",
+    "scan_and_detect_untrained_skills",
+]
