613ec9425e1f41024382142fdae3a8780508e220
R21-Volvik <cjimenez@r21digital.com>
2025-06-27 23:58:12 -0700

    Add rls mode handler

--- tests/test_rls_mode.py
+++ tests/test_rls_mode.py
@@ -1,4 +1,12 @@
+import importlib
+
 from android_ms11.modes import rls_mode
+
+
+def test_mode_handlers_contains_rls():
+    import src.main as main
+    main_mod = importlib.reload(main)
+    assert main_mod.MODE_HANDLERS["rls"] is rls_mode.run
 
 
 def test_rls_mode_stub_runs_once(capsys):
