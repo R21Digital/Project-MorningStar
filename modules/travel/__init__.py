0297b0880ed707c62f4dc9804587015d54a87845
R21-Volvik <cjimenez@r21digital.com>
2025-06-28 17:19:25 -0700

    Add location selector using shuttle travel

--- modules/travel/__init__.py
+++ modules/travel/__init__.py
@@ -1,5 +1,6 @@
 """Travel helpers package."""
 
 from .trainer_travel import travel_to_trainer
+from .location_selector import select_target
 
-__all__ = ["travel_to_trainer"]
+__all__ = ["travel_to_trainer", "select_target"]
