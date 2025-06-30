1aab443068eb0ea5ef8da99593789f6757c8ecbc
R21-Volvik <cjimenez@r21digital.com>
2025-06-30 15:40:51 -0700

    Add distance field to trainers and parse it

--- tests/core/test_train_manager.py
+++ tests/core/test_train_manager.py
@@ -9,3 +9,10 @@ def test_train_missing_skills(monkeypatch):
     tm = TrainManager(build_path='config/builds/rifleman_medic.json', trainer_db='data/trainers_simple.json')
     tm.train_missing_skills('tatooine')
     assert "science_medic_novice" in tm.trained_skills
+
+
+def test_load_trainers_distance_field():
+    tm = TrainManager(build_path='config/builds/rifleman_medic.json', trainer_db='data/trainers_simple.json')
+    trainers = tm.load_trainers()
+    assert all('distance_to_city' in t for t in trainers)
+    assert all(isinstance(t['distance_to_city'], float) for t in trainers)
